517831cc22783196aca4245c9d0a77333aea7c55
Vipul Wadekar <vip.wadekar@gmail.com>
2022-02-20 21:45:46 +0530

    changed Alert Button (#1)

--- textutils/src/components/Alert.js
+++ textutils/src/components/Alert.js
@@ -10,7 +10,7 @@ function Alert(props) {
 
         props.alert && <div className={`alert alert-${props.alert.type} alert-dismissible fade show`} role="alert">
         <strong>{capatalize(props.alert.type)}</strong> {props.alert.msg}
-        {/* <button type="button" className="btn-close" data-bs-dismiss="alert" aria-label="Close"></button> */}
+        <button type="button" className="btn-close" data-bs-dismiss="alert" aria-label="Close"></button> 
       </div>
 
     )
